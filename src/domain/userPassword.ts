68033788f7eab59478a85e6ab03f293a2150dbe7
Tomás Lopes <1211289@isep.ipp.pt>
2023-10-08 22:48:44 +0100

    chore: updated bcrypt

--- src/domain/userPassword.ts
+++ src/domain/userPassword.ts
@@ -1,7 +1,8 @@
-import { ValueObject } from "../core/domain/ValueObject";
-import { Result } from "../core/logic/Result";
-import { Guard } from "../core/logic/Guard";
-import * as bcrypt from 'bcrypt-nodejs';
+import { ValueObject } from '../core/domain/ValueObject';
+import { Guard } from '../core/logic/Guard';
+import { Result } from '../core/logic/Result';
+
+import bcrypt from 'bcrypt';
 
 interface UserPasswordProps {
   value: string;
@@ -9,21 +10,21 @@ interface UserPasswordProps {
 }
 
 export class UserPassword extends ValueObject<UserPasswordProps> {
-  
-  get value (): string {
+  private readonly SALT_ROUNDS = 10;
+
+  get value(): string {
     return this.props.value;
   }
 
-  private constructor (props) {
-    super(props)
+  private constructor(props) {
+    super(props);
   }
 
-    /**
+  /**
    * @method comparePassword
    * @desc Compares as plain-text and hashed password.
    */
-
-  public async comparePassword (plainTextPassword: string): Promise<boolean> {
+  public async comparePassword(plainTextPassword: string): Promise<boolean> {
     let hashed: string;
     if (this.isAlreadyHashed()) {
       hashed = this.props.value;
@@ -33,60 +34,52 @@ export class UserPassword extends ValueObject<UserPasswordProps> {
     }
   }
 
-  private bcryptCompare (plainText: string, hashed: string): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      bcrypt.compare(plainText, hashed, (err, compareResult) => {
-        if (err) return resolve(false);
-        return resolve(compareResult);
-      })
-    })
+  private bcryptCompare(plainText: string, hashed: string): Promise<boolean> {
+    return bcrypt.compare(plainText, hashed);
   }
 
-  public isAlreadyHashed (): boolean {
+  public isAlreadyHashed(): boolean {
     return this.props.hashed;
   }
-  
-  private hashPassword (password: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      bcrypt.hash(password, null, null, (err, hash) => {
-        if (err) return reject(err);
-        resolve(hash)
-      })
-    })
+
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, this.SALT_ROUNDS);
   }
 
-  public getHashedValue (): Promise<string> {
-    return new Promise((resolve) => {
+  public getHashedValue(): Promise<string> {
+    return new Promise(resolve => {
       if (this.isAlreadyHashed()) {
         return resolve(this.props.value);
       } else {
-        return resolve(this.hashPassword(this.props.value))
+        return resolve(this.hashPassword(this.props.value));
       }
-    })
+    });
   }
 
-  public static isAppropriateLength (value: string): boolean {
+  public static isAppropriateLength(value: string): boolean {
     return value.length >= 8;
   }
 
-  public static create (props: UserPasswordProps): Result<UserPassword> {
+  public static create(props: UserPasswordProps): Result<UserPassword> {
     const propsResult = Guard.againstNullOrUndefined(props.value, 'password');
 
     if (!propsResult.succeeded) {
       return Result.fail<UserPassword>(propsResult.message);
     } else {
-
       if (!props.hashed) {
-        if (!this.isAppropriateLength(props.value)
-        ) {
-          return Result.fail<UserPassword>('Password doesnt meet criteria [1 uppercase, 1 lowercase, one digit or symbol and 8 chars min].');
+        if (!this.isAppropriateLength(props.value)) {
+          return Result.fail<UserPassword>(
+            "Password doesn't meet criteria [1 uppercase, 1 lowercase, one digit or symbol and 8 chars min].",
+          );
         }
       }
 
-      return Result.ok<UserPassword>(new UserPassword({
-        value: props.value,
-        hashed: !!props.hashed === true
-      }));
+      return Result.ok<UserPassword>(
+        new UserPassword({
+          value: props.value,
+          hashed: !!props.hashed,
+        }),
+      );
     }
   }
 }
