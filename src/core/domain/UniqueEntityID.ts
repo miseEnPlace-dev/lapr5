f68080d8e1df480d62bc388c4476f783e72fb981
Tomás Lopes <1211289@isep.ipp.pt>
2023-10-09 09:40:52 +0100

    chore: added missing uuid dependency

--- src/core/domain/UniqueEntityID.ts
+++ src/core/domain/UniqueEntityID.ts
@@ -1,10 +1,8 @@
+import { v4 as uuid } from 'uuid';
+import { Identifier } from './Identifier';
 
-// remove by JRT : import uuid from 'uuid/v4';
-const uuid = require('uuid').v4  // Added by JRT
-import { Identifier } from './Identifier'
-
-export class UniqueEntityID extends Identifier<string | number>{
-  constructor (id?: string | number) {
-    super(id ? id : uuid())
+export class UniqueEntityID extends Identifier<string | number> {
+  constructor(id?: string | number) {
+    super(id ? id : uuid());
   }
 }
