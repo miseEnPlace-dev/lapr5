38c3787b8263523c759cfcdd333dbc43a7c03367
Ricardo M <rmoreiradev@gmail.com>
2023-10-16 09:38:46 +0100

    chore: added user phone number

---
+++ md-devices/src/domain/userPhoneNumber.ts
@@ -0,0 +1,34 @@
+import { ValueObject } from '../core/domain/ValueObject';
+import { Result } from '../core/logic/Result';
+import { Guard } from '../core/logic/Guard';
+
+interface UserPhoneNumberProps {
+  value: string;
+}
+
+export class UserPhoneNumber extends ValueObject<UserPhoneNumberProps> {
+  get value(): string {
+    return this.props.value;
+  }
+
+  private constructor(props: UserPhoneNumberProps) {
+    super(props);
+  }
+
+  public static create(phoneNumber: string): Result<UserPhoneNumber> {
+    const guardResult = Guard.againstNullOrUndefined(phoneNumber, 'phoneNumber');
+
+    // regex for portuguese phone numbers
+    const regex = /^[9][1236]\d{7}$/;
+
+    if (!guardResult.succeeded) {
+      return Result.fail<UserPhoneNumber>(guardResult.message);
+    }
+
+    if (!regex.test(phoneNumber)) {
+      return Result.fail<UserPhoneNumber>('Invalid phone number');
+    }
+
+    return Result.ok<UserPhoneNumber>(new UserPhoneNumber({ value: phoneNumber }));
+  }
+}
