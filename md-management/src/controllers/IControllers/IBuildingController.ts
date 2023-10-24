0e930e291c9679b07c5b71d23347a8ce499378df
Carlos Lopes <1211277@isep.ipp.pt>
2023-10-24 19:53:13 +0100

    feat(us-160): added update to controller

--- md-management/src/controllers/IControllers/IBuildingController.ts
+++ md-management/src/controllers/IControllers/IBuildingController.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export default interface IBuildingController {
   getBuildings(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   createBuilding(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+  updateBuilding(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   getBuildingsWithMinMaxFloors(
     req: Request,
     res: Response,
