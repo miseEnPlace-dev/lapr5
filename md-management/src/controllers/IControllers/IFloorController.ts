204052c23622675f003e3f60a7b4dcc4c08e3530
Carlos Lopes <1211277@isep.ipp.pt>
2023-10-25 18:53:09 +0100

    feat(us-230): uploadMap to controller

--- md-management/src/controllers/IControllers/IFloorController.ts
+++ md-management/src/controllers/IControllers/IFloorController.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export default interface IFloorController {
   createFloor(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   updateFloor(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+  uploadMap(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   getBuildingFloors(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
   getFloorsWithElevator(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
 }
