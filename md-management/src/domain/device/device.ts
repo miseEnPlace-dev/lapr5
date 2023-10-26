38b43ffe5fe65fd08bca8ba08187530e164106f6
André Barros <1211299@isep.ipp.pt>
2023-10-26 16:57:22 +0100

    feat: added device domain classes

---
+++ md-management/src/domain/device/device.ts
@@ -0,0 +1,63 @@
+import { Guard } from '@/core/logic/Guard';
+import { AggregateRoot } from '../../core/domain/AggregateRoot';
+import { UniqueEntityID } from '../../core/domain/UniqueEntityID';
+import { DeviceModel } from '../device-model/device-model';
+import { DeviceDescription } from './deviceDescription';
+import { DeviceNickname } from './deviceNickname';
+import { DeviceSerialNumber } from './deviceSerialNumber';
+import { Result } from '@/core/logic/Result';
+
+interface DeviceProps {
+  nickname: DeviceNickname;
+  description?: DeviceDescription;
+  serialNumber: DeviceSerialNumber;
+  model: DeviceModel;
+  isAvailable: boolean;
+}
+
+export class Device extends AggregateRoot<DeviceProps> {
+  get id(): UniqueEntityID {
+    return this._id;
+  }
+
+  get nickname(): DeviceNickname {
+    return this.props.nickname;
+  }
+
+  get description(): DeviceDescription | undefined {
+    return this.props.description;
+  }
+
+  get serialNumber(): DeviceSerialNumber {
+    return this.props.serialNumber;
+  }
+
+  get model(): DeviceModel {
+    return this.props.model;
+  }
+
+  get isAvailable(): boolean {
+    return this.props.isAvailable;
+  }
+
+  constructor(props: DeviceProps, id?: UniqueEntityID) {
+    super(props, id);
+  }
+
+  public static create(props: DeviceProps, id?: UniqueEntityID): Result<Device> {
+    const guardedProps = [
+      { argument: props.nickname, argumentName: 'nickname' },
+      { argument: props.serialNumber, argumentName: 'serialNumber' },
+      { argument: props.model, argumentName: 'model' },
+      { argument: props.isAvailable, argumentName: 'isAvailable' }
+    ];
+
+    const guardResult = Guard.againstNullOrUndefinedBulk(guardedProps);
+
+    if (!guardResult.succeeded) return Result.fail<Device>(guardResult.message);
+
+    const device = new Device({ ...props }, id);
+
+    return Result.ok<Device>(device);
+  }
+}
---
+++ md-management/src/domain/device/deviceSerialNumber.ts
@@ -0,0 +1,20 @@
+import { ValueObject } from '../../core/domain/ValueObject';
+import { Result } from '../../core/logic/Result';
+
+interface DeviceSerialNumberProps {
+  [key: string]: string;
+  value: string;
+}
+
+export class DeviceSerialNumber extends ValueObject<DeviceSerialNumberProps> {
+  get value(): string {
+    return this.props.value;
+  }
+
+  public static create(value: string): Result<DeviceSerialNumber> {
+    if (value.length > 50)
+      return Result.fail<DeviceSerialNumber>('Device serial number must be 50 characters or less');
+
+    return Result.ok<DeviceSerialNumber>(new DeviceSerialNumber({ value }));
+  }
+}
