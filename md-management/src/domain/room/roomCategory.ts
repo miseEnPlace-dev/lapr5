72887b2388e769465303e0de2fb1532c5d3b7561
André Barros <1211299@isep.ipp.pt>
2023-10-26 13:31:59 +0100

    fix: added missing room category to room domain class

--- md-management/src/domain/room/roomCategory.ts
+++ md-management/src/domain/room/roomCategory.ts
@@ -1,6 +1,32 @@
-export enum RoomCategory {
-  OFFICE = 'OFFICE',
-  LAB = 'LAB',
-  MEETING_ROOM = 'MEETING_ROOM',
-  CLASSROOM = 'CLASSROOM'
+import { ValueObject } from '../../core/domain/ValueObject';
+import { Result } from '../../core/logic/Result';
+import { RoomCategoryEnum } from './roomCategoryEnum';
+
+interface RoomCategoryProps {
+  [key: string]: RoomCategoryEnum;
+  value: RoomCategoryEnum;
+}
+
+export class RoomCategory extends ValueObject<RoomCategoryProps> {
+  get value(): string {
+    return this.props.value;
+  }
+
+  private constructor(props: RoomCategoryProps) {
+    super(props);
+  }
+
+  public static create(category: string): Result<RoomCategory> {
+    if (
+      category !== RoomCategoryEnum.OFFICE &&
+      category !== RoomCategoryEnum.LAB &&
+      category !== RoomCategoryEnum.MEETING_ROOM &&
+      category !== RoomCategoryEnum.CLASSROOM
+    )
+      return Result.fail<RoomCategory>(
+        'Room category must be OFFICE, LAB, MEETING_ROOM or CLASSROOM'
+      );
+
+    return Result.ok<RoomCategory>(new RoomCategory({ value: category }));
+  }
 }
