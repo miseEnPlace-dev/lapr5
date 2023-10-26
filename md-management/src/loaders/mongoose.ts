cf4c15704a0327035cf1be665065aefea7319123
Tomás Lopes <1211289@isep.ipp.pt>
2023-10-26 09:09:48 +0100

    fix: removed depracated warning in mongoose

--- md-management/src/loaders/mongoose.ts
+++ md-management/src/loaders/mongoose.ts
@@ -1,8 +1,9 @@
-import mongoose from 'mongoose';
-import { Db } from 'mongodb';
 import config from '@/config.mjs';
+import { Db } from 'mongodb';
+import mongoose from 'mongoose';
 
 export default async (): Promise<Db> => {
+  mongoose.set('strictQuery', false);
   const connection = await mongoose.connect(config.databaseURL);
   return connection.connection.db;
 };
