2651f4035026ab6abc3e53631c2666f772d5a1ca
Carlos Lopes <1211277@isep.ipp.pt>
2023-11-02 17:02:50 +0000

    test: elevator branding brand and model lenght

--- md-management/tests/unit/domain/elevator/elevatorBranding.test.ts
+++ md-management/tests/unit/domain/elevator/elevatorBranding.test.ts
@@ -2,7 +2,25 @@ import { describe, expect, it } from 'vitest';
 import { ElevatorBranding } from '../../../../src/domain/elevator/elevatorBranding';
 
 describe('ElevatorBranding', () => {
-  it('should not allow to create a branding with more than 50 characters', () => {
+  it('should not allow to create a branding with a brand with more than 50 characters', () => {
+    const result = ElevatorBranding.create(
+      [...Array(51)].map(() => 'a').join(''),
+      [...Array(50)].map(() => 'a').join('')
+    );
+
+    expect(result.isFailure).toBe(true);
+  });
+
+  it('should not allow to create a branding with a model with more than 50 characters', () => {
+    const result = ElevatorBranding.create(
+      [...Array(50)].map(() => 'a').join(''),
+      [...Array(51)].map(() => 'a').join('')
+    );
+
+    expect(result.isFailure).toBe(true);
+  });
+
+  it('should not allow to create a branding with a brand and model with more than 50 characters', () => {
     const result = ElevatorBranding.create(
       [...Array(51)].map(() => 'a').join(''),
       [...Array(51)].map(() => 'a').join('')
