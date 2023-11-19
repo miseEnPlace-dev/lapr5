06bb5360fc576b7200a5bfb1f9f904f77a648409
Tomás Lopes <1211289@isep.ipp.pt>
2023-11-19 23:43:49 +0000

    feat: added building connections

--- planning/algav.pl
+++ planning/algav.pl
@@ -75,6 +75,95 @@ m(6,7,0).
 m(7,7,0).
 m(8,7,1).
 
+liga(a,h).
+liga(b,g).
+liga(b,i).
+liga(g,h).
+liga(h,i).
+liga(i,j).
+
+pisos(a,[a1]).
+pisos(b,[b1,b2,b3,b4]).
+pisos(g,[g2,g3,g4]).
+pisos(h,[h1,h2,h3,h4]).
+pisos(i,[i1,i2,i3,i4]).
+pisos(j,[j1,j2,j3,j4]).
+
+elevador(b,[b1,b2,b3,b4]).
+elevador(g,[g2,g3,g4]).
+elevador(i,[i1,i2,i3,i4]).
+elevador(j,[j1,j2,j3,j4]).
+
+corredor(a,h,a1,h2).
+corredor(b,g,b2,g2).
+corredor(b,g,b3,g3).
+corredor(b,i,b3,i3).
+corredor(g,h,g2,h2).
+corredor(g,h,g3,h3).
+corredor(h,i,h2,i2).
+corredor(i,j,i1,j1).
+corredor(i,j,i2,j2).
+corredor(i,j,i3,j3).
+
+caminho_edificios(EdOr,EdDest,LEdCam):-	caminho_edificios2(EdOr,EdDest,[EdOr],LEdCam).
+
+caminho_edificios2(EdX,EdX,LEdInv,LEdCam):-!,reverse(LEdInv,LEdCam).
+
+caminho_edificios2(EdAct,EdDest,LEdPassou,LEdCam):-
+	(liga(EdAct,EdInt);liga(EdInt,EdAct)), 
+	\+member(EdInt,LEdPassou),
+	caminho_edificios2(EdInt,EdDest,[EdInt|LEdPassou],LEdCam).
+
+todos_caminhos_edificios(EdOr,EdDest,LTCamEd):-
+	findall(LEdCam,caminho_edificios(EdOr,EdDest,LEdCam),LTCamEd).
+
+caminho_pisos(PisoOr,PisoDest,LEdCam,LLig):-pisos(EdOr,LPisosOr),member(PisoOr,LPisosOr),
+	pisos(EdDest,LPisosDest),member(PisoDest,LPisosDest),
+	caminho_edificios(EdOr,EdDest,LEdCam),
+	segue_pisos(PisoOr,PisoDest,LEdCam,LLig).
+segue_pisos(PisoDest,PisoDest,_,[]).
+segue_pisos(PisoDest1,PisoDest,[EdDest],[elev(PisoDest1,PisoDest)]):-
+	PisoDest\==PisoDest1,
+	elevador(EdDest,LPisos), member(PisoDest1,LPisos), member(PisoDest,LPisos).
+segue_pisos(PisoAct,PisoDest,[EdAct,EdSeg|LOutrosEd],[cor(PisoAct,PisoSeg)|LOutrasLig]):-
+	(corredor(EdAct,EdSeg,PisoAct,PisoSeg);corredor(EdSeg,EdAct,PisoSeg,PisoAct)),
+	segue_pisos(PisoSeg,PisoDest,[EdSeg|LOutrosEd],LOutrasLig).
+segue_pisos(PisoAct,PisoDest,[EdAct,EdSeg|LOutrosEd],[elev(PisoAct,PisoAct1),cor(PisoAct1,PisoSeg)|LOutrasLig]):-
+	(corredor(EdAct,EdSeg,PisoAct1,PisoSeg);corredor(EdSeg,EdAct,PisoSeg,PisoAct1)),
+	PisoAct1\==PisoAct,
+	elevador(EdAct,LPisos),member(PisoAct,LPisos),member(PisoAct1,LPisos),
+	segue_pisos(PisoSeg,PisoDest,[EdSeg|LOutrosEd],LOutrasLig).
+
+
+melhor_caminho_pisos_elevadores(PisoOr,PisoDest,LLigMelhor):-
+	findall(LLig,caminho_pisos(PisoOr,PisoDest,_,LLig),LLLig),
+	menos_elevadores(LLLig,LLigMelhor,_,_).
+
+menos_elevadores([LLig],LLig,NElev,NCor):-conta(LLig,NElev,NCor).
+menos_elevadores([LLig|OutrosLLig],LLigR,NElevR,NCorR):-
+	menos_elevadores(OutrosLLig,LLigM,NElev,NCor),
+	conta(LLig,NElev1,NCor1),
+	(((NElev1<NElev;(NElev1==NElev,NCor1<NCor)),!,
+	NElevR is NElev1, NCorR is NCor1,LLigR=LLig);
+	(NElevR is NElev,NCorR is NCor,LLigR=LLigM)).
+
+melhor_caminho_pisos_edificios(PisoOr,PisoDest,LLigMelhor):-
+	findall(LLig,caminho_pisos(PisoOr,PisoDest,_,LLig),LLLig),
+	menos_edificios(LLLig,LLigMelhor,_,_).
+
+menos_edificios([LLig],LLig,NElev,NCor):-conta(LLig,NElev,NCor).
+menos_edificios([LLig|OutrosLLig],LLigR,NElevR,NCorR):-
+	menos_edificios(OutrosLLig,LLigM,NElev,NCor),
+	conta(LLig,NElev1,NCor1),
+	(((NCor1<NCor;(NCor1==NCor,NElev1<NElev)),!,
+	NElevR is NElev1, NCorR is NCor1,LLigR=LLig);
+	(NElevR is NElev,NCorR is NCor,LLigR=LLigM)).
+
+conta([],0,0).
+conta([elev(_,_)|L],NElev,NCor):-conta(L,NElevL,NCor),NElev is NElevL+1.
+conta([cor(_,_)|L],NElev,NCor):-conta(L,NElev,NCorL),NCor is NCorL+1.
+
+
 cria_grafo(_,0):-!.
 cria_grafo(Col,Lin):-cria_grafo_lin(Col,Lin),Lin1 is Lin-1,cria_grafo(Col,Lin1).
 
