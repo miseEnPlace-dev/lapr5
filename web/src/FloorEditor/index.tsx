189da3c6ea6d809592364838028dbe3e978d0029
Carlos Lopes <1211277@isep.ipp.pt>
2023-11-21 21:29:48 +0000

    feat: building B floor 2 json

--- web/src/FloorEditor/index.tsx
+++ web/src/FloorEditor/index.tsx
@@ -7,6 +7,7 @@ import Orientation from "./orientation";
 import ThumbRaiser from "./thumb_raiser";
 
 const maps = [
+  "building-c-floor-2.json",
   "building-c-floor-1.json",
   "building-d-floor-1.json",
   "building-d-floor-2.json",
