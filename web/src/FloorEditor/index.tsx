d69b2b4fb86dd3aa49f28c46ecf0a07443ed90a8
André Barros <1211299@isep.ipp.pt>
2023-11-22 17:18:31 +0000

    feat: added building B first floor

--- web/src/FloorEditor/index.tsx
+++ web/src/FloorEditor/index.tsx
@@ -8,6 +8,7 @@ import ThumbRaiser from "./thumb_raiser";
 
 const maps = [
   "building-a-floor-1.json",
+  "building-b-floor-1.json",
   "building-c-floor-2.json",
   "building-c-floor-1.json",
   "building-d-floor-1.json",
