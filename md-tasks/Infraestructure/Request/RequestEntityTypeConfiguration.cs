d46cdd40f7ad69c32fb37522610ecd4d3661671a
André Barros <1211299@isep.ipp.pt>
2023-12-08 19:26:18 +0000

    feat: added request repository

---
+++ md-tasks/Infraestructure/Request/RequestEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+
+using DDDSample1.Domain.Requests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DDDSample1.Infrastructure.Requests
+{
+  internal class RequestEntityTypeConfiguration : IEntityTypeConfiguration<Request>
+  {
+    public void Configure(EntityTypeBuilder<Request> builder)
+    {
+      builder.HasKey(b => b.Id);
+      builder.OwnsOne(b => b.State);
+    }
+  }
+}
