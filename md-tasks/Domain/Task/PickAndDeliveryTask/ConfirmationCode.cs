2b84dbedbaad960c4e8bacba3e6d3534a9cb7541
André Barros <1211299@isep.ipp.pt>
2023-12-08 19:29:44 +0000

    chore: added validation to confirmation code

--- md-tasks/Domain/Task/PickAndDeliveryTask/ConfirmationCode.cs
+++ md-tasks/Domain/Task/PickAndDeliveryTask/ConfirmationCode.cs
@@ -10,12 +10,21 @@ namespace DDDSample1.Domain.Task.PickAndDeliveryTask
 
     private ConfirmationCode()
     {
-      Code = "Pending";
+      Code = "";
     }
 
     public ConfirmationCode(string code)
     {
+      ValidateCode(code);
       Code = code;
+    }
+
+    private void ValidateCode(string code)
+    {
+      if (code.Length > 4 && code.Length < 6)
+      {
+        throw new BusinessRuleValidationException("Confirmation code must at least 4 characters and at most 6 characters.");
+      }
     }
 
     protected override IEnumerable<object> GetEqualityComponents()
