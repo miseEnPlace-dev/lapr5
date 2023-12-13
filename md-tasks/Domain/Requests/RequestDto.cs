15738628d6763375875dce6387332981b2349266
André Barros <1211299@isep.ipp.pt>
2023-12-13 09:29:08 +0000

    fix: requestDTO LAPR5-85

--- md-tasks/Domain/Requests/RequestDto.cs
+++ md-tasks/Domain/Requests/RequestDto.cs
@@ -7,9 +7,15 @@ public class RequestDto
   public Guid Id { get; set; }
   public string State { get; set; }
 
-  public RequestDto(Guid Id, string State)
+  public string UserId { get; set; }
+
+  public string DeviceModelId { get; set; }
+
+  public RequestDto(Guid Id, string State, string UserId, string DeviceModelId)
   {
     this.Id = Id;
     this.State = State;
+    this.UserId = UserId;
+    this.DeviceModelId = DeviceModelId;
   }
 }
