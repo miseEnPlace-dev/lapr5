d4b7f9cb99a736bb98cc527de2e5c40a6e96e54e
Tomás Lopes <1211289@isep.ipp.pt>
2023-12-17 16:11:39 +0000

    fix: fixed floor map 2d visualizer styling

--- web/src/components/FloorMapVisualizer/index.tsx
+++ web/src/components/FloorMapVisualizer/index.tsx
@@ -25,7 +25,7 @@ const FloorMapVisualizer: React.FC<FloorMapVisualizerProps> = ({
         {Array.from(Array(map[0].length + 1).keys()).map((i) => (
           <div
             key={i.toString()}
-            className="h-8 w-8 text-center align-middle text-xl font-bold"
+            className="flex h-10 w-10 items-center justify-center text-xl font-bold"
           >
             {i !== 0 && i}
           </div>
@@ -34,15 +34,15 @@ const FloorMapVisualizer: React.FC<FloorMapVisualizerProps> = ({
       {map.map((row, i) => (
         <div
           key={row.toString()}
-          className="flex h-8 items-center justify-center"
+          className="flex h-10 items-center justify-center"
         >
-          <div className="h-8 w-8 text-center align-middle text-xl font-bold">
+          <div className="flex h-10 w-10 items-center justify-center text-xl font-bold">
             {i + 1}
           </div>
           {row.map((cell, j) => (
             <div
               key={cell.toString()}
-              className={`relative h-8 w-8 select-none border border-slate-400 text-center align-middle text-xl font-bold ${
+              className={`relative flex h-10 w-10 select-none items-center justify-center border border-slate-400 text-xl font-bold ${
                 cell === 1 || cell === 2 || cell === 3
                   ? "bg-gray-800 hover:bg-slate-900"
                   : i === map.length - 1 && j === row.length - 1
@@ -67,7 +67,7 @@ const FloorMapVisualizer: React.FC<FloorMapVisualizerProps> = ({
                 ? "D"
                 : ""}
               {activeCell[0] === j && activeCell[1] === i && (
-                <div className="absolute -top-10 left-1/2 h-8 w-24 -translate-x-1/2 rounded-full bg-slate-300">
+                <div className="absolute -top-10 left-1/2 flex h-8 w-24 -translate-x-1/2 items-center justify-center rounded-full bg-slate-300">
                   {i + 1}, {j + 1}
                 </div>
               )}
