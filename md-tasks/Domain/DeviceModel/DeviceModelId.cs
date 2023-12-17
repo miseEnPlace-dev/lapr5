f6888878b8fcf04379daccb87b97f7d4d61092d8
André Barros <1211299@isep.ipp.pt>
2023-12-17 17:30:23 +0000

    fix: added validation to device model id

--- md-tasks/Domain/DeviceModel/DeviceModelId.cs
+++ md-tasks/Domain/DeviceModel/DeviceModelId.cs
@@ -6,12 +6,27 @@ namespace DDDSample1.Domain.DeviceModel
 {
   public class DeviceModelId : EntityId
   {
-
     [JsonConstructor]
     public DeviceModelId(Guid value) : base(value) { }
 
-    public DeviceModelId(string value) : base(value) { }
+    [JsonConstructor]
+    public DeviceModelId(string value) : base(ParseGuid(value)) { }
 
     public DeviceModelId() : base(Guid.NewGuid()) { }
+
+    private static Guid ParseGuid(string value)
+    {
+      if (string.IsNullOrEmpty(value))
+      {
+        throw new ArgumentException("DeviceModelId string cannot be null or empty.");
+      }
+
+      if (Guid.TryParse(value, out var guidValue))
+      {
+        return guidValue;
+      }
+
+      throw new ArgumentException("Invalid DeviceModelId format. It should be a valid GUID string.");
+    }
   }
 }
