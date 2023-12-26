71ef893b67cb997b1a8fb9fe1e0c4536a256e4e7
André Barros <1211299@isep.ipp.pt>
2023-12-26 12:00:13 +0000

    fix: pickAndDelivery entity LAPR5-85

--- md-tasks/Infraestructure/Task/PickAndDeliveryTaskEntityTypeConfiguration.cs
+++ md-tasks/Infraestructure/Task/PickAndDeliveryTaskEntityTypeConfiguration.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using DDDSample1.Domain.DeviceTasks.PickAndDeliveryTask;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using DDDSample1.Domain.User;
+using DDDSample1.Domain.Room;
 
 namespace DDDSample1.Infrastructure.Tasks;
 
@@ -14,8 +15,8 @@ internal class PickAndDeliveryTaskEntityTypeConfiguration : IEntityTypeConfigura
     //builder.Property(b => b.Id);
     builder.OwnsOne(b => b.ConfirmationCode);
     builder.Property(b => b.Description).HasConversion(b => b.Value, b => new TaskDescription(b));
-    builder.Property(b => b.PickupRoomId);
-    builder.Property(b => b.DeliveryRoomId);
+    builder.Property(b => b.PickupRoomId).HasConversion(b => b.Value, b => new RoomId(b));
+    builder.Property(b => b.DeliveryRoomId).HasConversion(b => b.Value, b => new RoomId(b));
     var userIdConverter = new ValueConverter<UserId, string>(
              v => v.Value,
              v => new UserId(v)
