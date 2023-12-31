84f09747f0c274a35df716bb289df67f5cbfa411
Ricardo M <rmoreiradev@gmail.com>
2023-12-31 02:02:15 +0000

    fix: fixed request date

--- web/src/utils/formatDate.ts
+++ web/src/utils/formatDate.ts
@@ -1,5 +1,9 @@
 export function formatDate(string: string) {
-  const date = new Date(string);
+  const dt = string.split(/-|\s|\//);
+  const date = new Date(
+    new Date(dt.slice(0, 3).reverse().join("-") + " " + dt[3])
+  );
+  console.log(string);
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
