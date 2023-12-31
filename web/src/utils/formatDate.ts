e723051058fa64aa0afef7deeb61e4348c188182
Ricardo M <rmoreiradev@gmail.com>
2023-12-31 02:03:08 +0000

    chore: removed print

--- web/src/utils/formatDate.ts
+++ web/src/utils/formatDate.ts
@@ -3,7 +3,6 @@ export function formatDate(string: string) {
   const date = new Date(
     new Date(dt.slice(0, 3).reverse().join("-") + " " + dt[3])
   );
-  console.log(string);
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
